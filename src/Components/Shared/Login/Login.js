938f79b6a1dc3bf7e7fef1c474eef99359206e7d
Habib826 <habib826826@.com>
2021-05-29 14:37:01 +0600

    google sign in

--- src/Components/Shared/Login/Login.js
+++ src/Components/Shared/Login/Login.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "../firebase.config"
 import './Login.css'
 
+firebase.initializeApp(firebaseConfig);
 const Login = () => {
+    //sign in with google
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const handleGoogleSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                const credential = result.credential;
+                const token = credential.accessToken;
+                const { displayName, email } = result.user;
+                console.log(displayName, email);
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    }
+
     return (
         <section className="login">
             <div className="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
@@ -17,9 +37,9 @@ const Login = () => {
                                 <div className="row mb-4 px-3">
                                     <h6 className="mb-0 mr-4 mt-2">Sign in with</h6>
                                     <div className="facebook text-center mr-3">
-                                        <div className="fab fa-google"></div>
+                                        <div onClick={handleGoogleSignIn} className="fab fa-google"></div>
                                     </div>
-                                    
+
                                 </div>
                                 <div className="row px-3 mb-4">
                                     <div className="line"></div> <small className="or text-center">Or</small>
