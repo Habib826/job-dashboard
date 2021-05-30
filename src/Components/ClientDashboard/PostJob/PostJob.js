8a36e272e0a7496e72a46a94002a48949ad9fbc4
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-05-30 10:38:48 -0600

    Add Job post component

---
+++ src/Components/ClientDashboard/PostJob/PostJob.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+
+const PostJob = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <h2 className="text-center">Post Your Job Here</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        {/* register your input into the hook by invoking the "register" function */}
+                        <input  {...register("jobTitle", { required: true })} type="text" placeholder="Enter job title" />
+                        {errors.jobTitle && <span>This field is required</span>}
+                        {/* include validation with required or other standard HTML validation rules */}
+                        <input {...register("location", { required: true })} type="text" placeholder="Enter job location" />
+                        {/* errors will return when field validation fails  */}
+                        {errors.exampleRequired && <span>This field is required</span>}
+                        <input {...register("company", { required: true })} type="text" placeholder="Enter your company name" />
+                        {/* errors will return when field validation fails  */}
+                        {errors.exampleRequired && <span>This field is required</span>}
+                        <input {...register("description", { required: true })} type="text" placeholder="Enterjob description" />
+                        {/* errors will return when field validation fails  */}
+                        {errors.exampleRequired && <span>This field is required</span>}
+                        <input type="submit" className="btn btn-primary"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PostJob;
